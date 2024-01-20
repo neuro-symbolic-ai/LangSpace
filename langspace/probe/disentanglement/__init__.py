c4f4f809ba5f429a479682667f4b4440fa1df7e4
Danilo S. Carvalho <danilo.carvalho@manchester.ac.uk>
2024-01-20 18:13:36 +0000

    - DisentanglementProbe docstrings.

--- langspace/probe/disentanglement/__init__.py
+++ langspace/probe/disentanglement/__init__.py
@@ -7,9 +7,28 @@ from .. import LatentSpaceProbe
 
 
 class DisentanglementProbe(LatentSpaceProbe):
+    """
+    Class for probing the disentanglement of the latent space of a language VAE.
+    """
     def __init__(self, model: LangVAE, data: Iterable[Union[str, Sentence]], sample_size: int,
                  metrics: List[DisentanglementMetrics], gen_factors: dict):
+        """
+        Initialize the DisentanglementProbe.
+
+        Args:
+            model (LangVAE): The language model to probe.
+            data (Iterable[Union[str, Sentence]]): The data to use for probing.
+            sample_size (int): The number of data points to use for probing.
+            metrics (List[DisentanglementMetrics]): A list of disentanglement metrics to compute.
+            gen_factors (dict): The generative factors to probe with.
+        """
         super(DisentanglementProbe, self).__init__(model, data, sample_size)
 
     def report(self) -> DataFrame:
+        """
+        Generate a report from the probe.
+
+        Returns:
+            DataFrame: The generated report.
+        """
         pass
